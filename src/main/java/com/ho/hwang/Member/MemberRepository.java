10b5be8b6628f6160f117bf53bbcc51cc170e6d4
HOHWANG <ghghkd52@naver.com>
2021-06-24 21:59:58 +0900

    feat : JapRepository 상속받은 Repository 생성(유저 조회 기능)

---
+++ src/main/java/com/ho/hwang/Member/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.ho.hwang.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberVo, Integer> {
+
+    public List<MemberVo> findById(String id);
+
+    public List<MemberVo> findByName(String name);
+
+    public List<MemberVo> findByNameLike(String keyword);
+}
