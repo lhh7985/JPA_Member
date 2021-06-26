0be7c19eecc3daab7fe4d2ed045c7891f890ed49
HOHWANG <ghghkd52@naver.com>
2021-06-26 22:14:48 +0900

    feat : @EntityScan("패키지경로")를 추가해야 Entity 클래스를 읽어 ddl-auto 가능

--- src/main/java/com/ho/hwang/Member/MemberVo.java
+++ src/main/java/com/ho/hwang/Member/MemberVo.java
@@ -4,19 +4,13 @@ package com.ho.hwang.Member;
 import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity(name = "member")
+@Entity
 public class MemberVo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long memberNo;
 
     private String id;
@@ -26,5 +20,9 @@ public class MemberVo {
     public MemberVo(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public MemberVo() {
+
     }
 }
--- src/main/java/com/ho/hwang/jpa/JpaApplication.java
+++ src/main/java/com/ho/hwang/jpa/JpaApplication.java
@@ -2,8 +2,10 @@ package com.ho.hwang.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.ho.hwang.Member")
 public class JpaApplication {
 
     public static void main(String[] args) {
