3819654d87312e27acb000509543f7b76a5cd1c1
HOHWANG <ghghkd52@naver.com>
2021-07-06 16:18:14 +0900

    feat : PostgreSQL table test(select, insert)

---
+++ src/main/java/com/ho/hwang/runner/PgSQLRunner.java
@@ -0,0 +1,36 @@
+package com.ho.hwang.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+public class PgSQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource datasource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = datasource.getConnection()) {
+            System.out.println(datasource.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            //create user table
+//            Statement statement = connection.createStatement();
+//            String sql = "create table users(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(id))";
+//            statement.executeUpdate(sql);
+        }
+        jdbcTemplate.execute("insert into users values(1, 'lhh')");
+    }
+}
