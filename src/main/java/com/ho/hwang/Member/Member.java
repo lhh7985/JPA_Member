f4229bc2c612eec2d55208d15fbb298e38f66db9
HOHWANG <ghghkd52@naver.com>
2021-07-11 23:20:34 +0900

    feat : 임베디드 타입 @Embeddable, 컴포지션(composition) 관계

--- src/main/java/com/ho/hwang/Member/Member.java
+++ src/main/java/com/ho/hwang/Member/Member.java
@@ -5,6 +5,7 @@ import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
@@ -14,12 +15,21 @@ public class Member {
     private long memberNo;
 
     private String id;
-    private String name;
+    private String username;
+    private String password;
+
+    @Temporal(TemporalType.DATE)
+    private Date created = new Date();
+
+    @Embedded
+    private Address address;
+
 
     @Builder
-    public Member(String id, String name) {
+    public Member(String id, String username,String password) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.password = password;
     }
 
     public Member() {
